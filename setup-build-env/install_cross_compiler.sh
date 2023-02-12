d6f0ca94c72622646ce0d150337d95df41ea8de6
Björn Töpel <bjorn@rivosinc.com>
2023-02-12 14:57:44 +0100

    install cross compiler
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- setup-build-env/install_cross_compiler.sh
+++ setup-build-env/install_cross_compiler.sh
@@ -5,29 +5,50 @@ source $(cd $(dirname $0) && pwd)/../helpers.sh
 
 foldable start install_cross_compiler "Installing cross-compilation GCC and libraries"
 
+if [[ $(uname -m) == "$ARCH" ]]; then
+	# Not cross-compiling
+	foldable end install_cross_compiler
+	exit 0
+fi
+
+# Only support x86-64 build platforms
+if [[ $(uname -m) != "x86_64" ]]; then
+	echo "Unsupport cross build platform" >&2
+	exit 1
+fi
+
 sudo sed -i 's/^deb/deb [arch=amd64]/g' /etc/apt/sources.list
 
-sudo dpkg --add-architecture riscv64
+# Translate to dpkg arch name
+debarch=$ARCH
+case "$ARCH" in
+aarch64)
+	debarch=arm64
+*)
+	;;
+esac
+
+sudo dpkg --add-architecture $debarch
 cat <<EOF | sudo tee -a /etc/apt/sources.list
-deb [arch=riscv64 signed-by="/usr/share/keyrings/ubuntu-archive-keyring.gpg"] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -c -s) main
-deb [arch=riscv64 signed-by="/usr/share/keyrings/ubuntu-archive-keyring.gpg"] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -c -s)-updates main
-deb [arch=riscv64 signed-by="/usr/share/keyrings/ubuntu-archive-keyring.gpg"] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -c -s)-security main
+deb [arch=$debarch signed-by="/usr/share/keyrings/ubuntu-archive-keyring.gpg"] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -c -s) main
+deb [arch=$debarch signed-by="/usr/share/keyrings/ubuntu-archive-keyring.gpg"] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -c -s)-updates main
+deb [arch=$debarch signed-by="/usr/share/keyrings/ubuntu-archive-keyring.gpg"] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -c -s)-security main
 EOF
 
 sudo apt-get update
 sudo apt-get install --yes --no-install-recommends \
-     g++-riscv64-linux-gnu \
-     gcc-riscv64-linux-gnu
+     g++-$ARCH-linux-gnu \
+     gcc-$ARCH-linux-gnu
 
 
 sudo apt-get install --yes --no-install-recommends \
-	libasound2-dev:riscv64 \
-	libc6-dev:riscv64 \
-	libcap-dev:riscv64 \
-	libcap-ng-dev:riscv64 \
-	libelf-dev:riscv64 \
-	libmnl-dev:riscv64 \
-	libnuma-dev:riscv64 \
-	libpopt-dev:riscv64
+        libasound2-dev:$ARCH \
+        libc6-dev:$ARCH \
+        libcap-dev:$ARCH \
+        libcap-ng-dev:$ARCH \
+        libelf-dev:$ARCH \
+        libmnl-dev:$ARCH \
+        libnuma-dev:$ARCH \
+        libpopt-dev:$ARCH
 
 foldable end install_cross_compiler
